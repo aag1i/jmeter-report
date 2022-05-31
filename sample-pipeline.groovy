39ffbaaecd4238c4471fc5fc57e617d43b86a605
aag1i <103033982+aag1i@users.noreply.github.com>
2022-05-31 11:07:10 +0800

    Update sample-pipeline.groovy

--- sample-pipeline.groovy
+++ sample-pipeline.groovy
@@ -1,33 +1,46 @@
 #!/usr/bin/env groovy
-node {
-	stage('Initialising') {
-        echo "###################################"
-		echo "Test automation project starting..."
-    }
-    stage('GitSCM') {
-        checkout scm: [
-                $class: 'GitSCM',
-                userRemoteConfigs: [
-                        [
-                                url: "https://github.com/aag1i/jmeter-report.git",
-                                credentialsId: "github-auth-agl"
-                        ]
-                ],
-                branches: [[name: "main"]]
-        ], poll: false
-    }
-    stage('Running Tests') {
-        dir("${WORKSPACE}") {
-			sh "/jenkins/tools/apache-jmeter-5.4.3/bin/jmeter.sh -n -t sample.jmx -l report.xml -p user.properties"
-        }
-    }
-	stage('Preparing Report') {
-        dir("${WORKSPACE}") {
-			sh "xsltproc report.xml sample.xslt > report.html"
-			archiveArtifacts artifacts: 'report.html'
-        }
-    }
-    stage('Complete') {
-        echo "Test completed..."
-    }
+pipeline
+{
+	parameters
+	{
+		choice(name: 'global.ENV', choices: ['DEV','UAT'], description: 'Pick the Environment')
+	}
+	
+	stages{
+		
+		agent any
+		
+		stage('Initialising') {
+			echo "###################################"
+			echo "Test automation project starting..."
+			echo "Environment : ${params.global.ENV}"
+		}
+		stage('GitSCM') {
+			checkout scm: [
+					$class: 'GitSCM',
+					userRemoteConfigs: [
+							[
+									url: "https://github.com/aag1i/jmeter-report.git",
+									credentialsId: "github-auth-agl"
+							]
+					],
+					branches: [[name: "main"]]
+			], poll: false
+		}
+		stage('Running Tests') {
+			dir("${WORKSPACE}") {
+				sh "/jenkins/tools/apache-jmeter-5.4.3/bin/jmeter.sh -n -t sample.jmx -l report.xml -p user.properties"
+			}
+		}
+		stage('Preparing Report') {
+			dir("${WORKSPACE}") {
+				sh "xsltproc report.xml sample.xslt > report.html"
+				archiveArtifacts artifacts: 'report.html'
+			}
+		}
+		stage('Complete') {
+			echo "Test completed..."
+		}
+	}
+
 }
